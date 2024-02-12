f43abea66a9ff96c09708eebf84e6a5b328d46d1
Teun Jansen <teunjansen91@gmail.com>
2024-02-12 12:57:44 +0100

    sellWeapon()

--- script.js
+++ script.js
@@ -80,10 +80,6 @@ function goCave() {
   update(locations[2]);
 }
 
-function fightDragon() {
-  console.log("Fighting dragon.");
-}
-
 function buyHealth() {
   if (gold >= 10) {
     gold -= 10;
@@ -109,7 +105,25 @@ function buyWeapon() {
     }
   } else {
     text.innerText = "You already have the most powerful weapon!";
+    button2.innerText = "Sell weapon for 15 gold";
+    button2.onclick = sellWeapon;
   }
+}
+
+function sellWeapon() {
+  if (inventory.length > 1) {
+    gold += 15;
+    goldText.innerText = gold;
+    let currentWeapon = inventory.shift();
+    text.innerText = "You sold a " + currentWeapon + ".";
+    text.innerText += " In your inventory you have: " + inventory;
+  } else {
+    text.innerText = "Don't sell your only weapon!";
+  }
+}
+
+function fightDragon() {
+  console.log("Fighting dragon.");
 }
 function fightSlime() {}
 function fightBeast() {}
